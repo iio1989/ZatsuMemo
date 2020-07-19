b403e7ba8133b30533b8bff402f24a5872585ab3
IioTatsuya <>
2020-07-20 02:21:28 +0900

    共通utils作成

---
+++ cmn/cmnUtils.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * $_POST[$pn]の形式で、パラメータを取得します。
+ * 尚、取得時にパラメータが存在するか確認します。
+ *
+ * @param [type] $pn パラメータ名
+ * @return パラメータ or null
+ */
+function getParam_checkExists($pn)
+{
+    $postName = $pn;
+    if (isset($_POST[$postName])) {
+        return $_POST[$postName];
+    } else {
+        return null;
+    }
+}
