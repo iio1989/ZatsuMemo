63b449c457999b393842730d23390e5284db8734
IioTatsuya <>
2020-09-17 19:20:24 +0900

    DBマイグレーションファイル カラム追加対応 配置場所変更

--- _dbMigration/ini.sql
+++ _dbMigration/ini.sql
@@ -1,7 +1,15 @@
+/************************************************
+* ZatsuMemo. DB migration.
+* version : 0.0.0
+* update : 2020/09/17
+************************************************/
+
+/** メモテーブル **/
 CREATE TABLE `memo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `memo_title` text,
-  `memo_body` text NOT NULL,
+  `title` text,
+  `body` text NOT NULL,
+  `category` int(11) DEFAULT NULL,  
   `create_user_id` int(11) DEFAULT NULL,
   `update_user_id` int(11) DEFAULT NULL,
   `create_date` datetime DEFAULT NULL,
@@ -12,11 +20,17 @@ CREATE TABLE `memo` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+/** ユーザーマスタ **/
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `show_id` varchar(45) DEFAULT NULL,
-  `user_name` varchar(45) DEFAULT NULL,
-  `user_pass` varchar(255) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
