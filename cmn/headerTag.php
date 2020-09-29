44f5d0c771a7b287fe41ad90bd6f8f8976024e2b
IioTatsuya <>
2020-09-29 19:34:32 +0900

    コンテンツセキュリティーポリシー対応

--- cmn/headerTag.php
+++ cmn/headerTag.php
@@ -1,2 +1,3 @@
 
 <link rel="stylesheet" type="text/css" href="../cmn/lib/bootstrap-4.5.0-dist/css/bootstrap.css?<?php echo getVersion();?>">
+<meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' <?php echo $_SERVER['HTTP_HOST'];?>; img-src 'self' data: <?php echo $_SERVER['HTTP_HOST'];?>;">
