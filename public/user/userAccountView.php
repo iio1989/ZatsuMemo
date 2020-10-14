7f7cac2e2de5d0ddf961a4e6c4fe3bbe7d38a7bf
IioTatsuya <>
2020-10-15 05:37:08 +0900

    ユーザーアカウントページ 文言修正

--- public/user/userAccountView.php
+++ public/user/userAccountView.php
@@ -34,16 +34,16 @@ if (isset($_POST["new_user_show_id"])) {
     <?php include('../cmn/header.php'); ?>
     <div class="user__account__form">
       <form class="form__id__name" method="post">
-        <div>ユーザーID：<input type="text" class="form-control" name="new_user_show_id" placeholder="ユーザーID" value="<?php echo issetConvertHsc($login_user, "show_id");?>"></div>
-        <div>ユーザー名：<input type="text" class="form-control" name="new_user_name" placeholder="ユーザー名" value="<?php echo issetConvertHsc($login_user, "name");?>"></div>
+        <div>・ユーザーID<input type="text" class="form-control" name="new_user_show_id" placeholder="ユーザーID" value="<?php echo issetConvertHsc($login_user, "show_id");?>"></div>
+        <div>・ユーザー名<input type="text" class="form-control" name="new_user_name" placeholder="ユーザー名" value="<?php echo issetConvertHsc($login_user, "name");?>"></div>
         <input type="submit" class="btn btn-primary" value="更新" style="width: 100%;">
       </form>
       <form class="form__pass" method="post">
         <?php if (isset($_POST["new_pass_err"])) { ?>
         <div>エラー パスワード新 と パスワード再の入力内容が異なります。</div>
         <?php } ?>
-        <div>パスワード(新)<input type="password" class="form-control" name="new_user_pass" placeholder="パスワード(新)"></div>
-        <div>パスワード(再)<input type="password" class="form-control" name="new_user_pass_re" placeholder="パスワード(再)"></div>
+        <div>・パスワード(新)<input type="password" class="form-control" name="new_user_pass" placeholder="パスワード(新)"></div>
+        <div>・パスワード(再)<input type="password" class="form-control" name="new_user_pass_re" placeholder="パスワード(再)"></div>
         <input type="submit" class="btn btn-primary" value="更新" style="width: 100%;">
       </form>
       <div class="text-center"><a href="../memo/memoListView.php">メモリスト画面へ移動する</a></div>
