844e094e691ff003cc083bdc8b0261815686ba28
IioTatsuya <>
2020-10-19 02:13:18 +0900

    ファビコン設定

--- public/cmn/headerTag.php
+++ public/cmn/headerTag.php
@@ -1,3 +1,4 @@
 
 <link rel="stylesheet" type="text/css" href="../cmn/lib/bootstrap-4.5.0-dist/css/bootstrap.min.css?<?php echo getVersion();?>">
+<link rel="shortcut icon" href="../cmn/image/favicon.ico?<?php echo getVersion();?>">
 <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' <?php echo $_SERVER['HTTP_HOST'];?>; img-src 'self' data: <?php echo $_SERVER['HTTP_HOST'];?>;">
